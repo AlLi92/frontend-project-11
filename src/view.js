62e5354b9ba8ae840ae1767b71d714c15192a242
AlLi92 <yelle34@yandex.ru>
2024-10-26 18:28:34 +0300

    Refactoring 6

--- src/view.js
+++ src/view.js
@@ -1,177 +1,146 @@
-/* eslint-disable no-param-reassign */
-import onChange from 'on-change';
-import { Modal } from 'bootstrap';
+import _ from 'lodash';
 
-const renderFeedback = (elements, i18n, watchedState) => {
-  elements.feedback.classList.add('text-danger');
-  const { error } = watchedState.form;
-  if (error) {
-    elements.feedback.textContent = i18n.t(`feedbacks.errors.${error}`);
-  } else {
-    elements.feedback.textContent = i18n.t('feedbacks.valid');
-    elements.feedback.style.color = 'green';
-    elements.feedback.classList.remove('text-danger');
-  }
-};
-
-const renderStatus = (elements, watchedState) => {
-  switch (watchedState.form.status) {
-    case 'finished':
-      elements.input.value = '';
-      elements.input.focus();
-      elements.add.removeAttribute('disabled');
-      break;
-    case 'sending':
-      elements.add.setAttribute('disabled', true);
-      break;
-    case 'failed':
-      elements.add.removeAttribute('disabled');
-      break;
-    default:
-      break;
-  }
-};
-const renderFeeds = (elements, i18n, watchedState) => {
-  elements.feeds.innerHTML = '';
-  const feedsCard = document.createElement('div');
-  feedsCard.classList.add('card', 'border-0');
-  elements.feeds.append(feedsCard);
-  const feedsBody = document.createElement('div');
-  feedsBody.classList.add('card-body');
-  feedsCard.append(feedsBody);
-  const feedsTitle = document.createElement('h2');
-  feedsTitle.classList.add('card-title', 'h4');
-  feedsTitle.textContent = i18n.t('feeds');
-  feedsBody.append(feedsTitle);
-  const ul = document.createElement('ul');
-  ul.classList.add('list-group', 'border-0', 'rounded-0');
-  feedsCard.append(ul);
-  watchedState.feeds.forEach((feed) => {
-    const li = document.createElement('li');
-    li.classList.add('list-group-item', 'border-0', 'border-end-0');
-    ul.append(li);
-    const h3 = document.createElement('h3');
-    h3.classList.add('h6', 'm-0');
-    h3.textContent = feed.text;
-    const p = document.createElement('p');
-    p.classList.add('m-0', 'small', 'text-black-50');
-    p.textContent = feed.description;
-    li.append(h3, p);
-  });
-};
-
-const renderPosts = (elements, i18n, watchedState) => {
-  elements.posts.innerHTML = '';
-  const postsCard = document.createElement('div');
-  postsCard.classList.add('card', 'border-0');
-  elements.posts.append(postsCard);
-  const postsBody = document.createElement('div');
-  postsBody.classList.add('card-body');
-  postsCard.append(postsBody);
-  const postsTitle = document.createElement('h2');
-  postsTitle.classList.add('card-title', 'h4');
-  postsTitle.textContent = i18n.t('posts');
-  postsBody.append(postsTitle);
-  const ul = document.createElement('ul');
-  ul.classList.add('list-group', 'border-0', 'rounded-0');
-  postsCard.append(ul);
-
-  watchedState.posts.forEach((post) => {
-    const {
-      id, text, link,
-    } = post;
-    const li = document.createElement('li');
-    const button = document.createElement('button');
-    const a = document.createElement('a');
-
-    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
-    ul.prepend(li);
-
-    a.href = link;
-    a.textContent = text;
-    a.target = '_blank';
-    a.setAttribute('rel', 'noopener noreferrer');
-    a.setAttribute('data-post-id', id);
-    if (watchedState.ulStateOpened.includes(id)) {
-      a.classList.add('fw-normal');
-    } else {
-      a.classList.add('fw-bold');
-    }
-
-    button.type = 'button';
-    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-    button.textContent = i18n.t('button');
-    button.setAttribute('data-post-id', id);
-    li.append(a, button);
-  });
-
-  ul.addEventListener('click', (e) => {
-    const button = e.target.closest('button');
-    const a = e.target.closest('a');
-
-    if (button) {
-      watchedState.ulStateOpened.push(button.dataset.postId);
-      const [targetPost] = watchedState.posts.filter((post) => post.id === button.dataset.postId);
-      const modal = new Modal(elements.modal);
-      elements.modalTitle.textContent = targetPost.text;
-      elements.modalBody.textContent = targetPost.description;
-      elements.modalArticle.href = targetPost.link;
-      modal.show();
-    } else if (a) {
-      watchedState.ulStateOpened.push(a.dataset.postId);
-      a.classList.remove('fw-bold');
-      a.classList.add('fw-normal');
-    }
-  });
-};
-
-const render = (elements, i18n) => {
-  elements.title.textContent = i18n.t('title');
-  elements.subtitle.textContent = i18n.t('subtitle');
-  elements.label.textContent = i18n.t('label');
-  elements.example.textContent = i18n.t('example');
-  elements.add.textContent = i18n.t('add');
-  elements.fullArticle.textContent = i18n.t('modal.fullArticle');
-  elements.buttonClose.textContent = i18n.t('modal.buttonClose');
-};
-
-export default (details, i18next, state) => {
-  const renderValid = (elements, watchedState) => {
-    if (watchedState.form.valid) {
-      elements.input.classList.remove('is-invalid');
-    } else {
-      elements.input.classList.add('is-invalid');
-    }
+const renderErrors = (errors, i18nextInstance) => {
+  const errorP = document.querySelector('.feedback');
+  const input = document.querySelector('#url-input');
+  const setErrorState = (message) => {
+    input.classList.add('is-invalid');
+    errorP.textContent = message;
+    errorP.classList.add('text-danger');
   };
-
-  const watchedState = onChange(state, (path) => {
-    console.log(path);
-    switch (path) {
-      case 'lng':
-        render(details, i18next);
-        break;
-      case 'form.status':
-        renderStatus(details, watchedState);
-        break;
-      case 'form.error':
-        renderFeedback(details, i18next, watchedState);
-        break;
-      case 'form.valid':
-        renderValid(details, watchedState);
-        break;
-      case 'posts':
-        renderPosts(details, i18next, watchedState);
-        break;
-      case 'ulStateOpened':
-        renderPosts(details, i18next, watchedState);
-        break;
-      case 'feeds':
-        renderFeeds(details, i18next, watchedState);
-        renderFeedback(details, i18next, watchedState);
+  input.classList.remove('is-invalid');
+  errorP.classList.remove('text-danger', 'text-success');
+  if (errors.activeUrl) {
+    setErrorState(errors.activeUrl.message);
+  } else if (errors.isRSSUrlError) {
+    setErrorState(i18nextInstance.t('notValidRSS'));
+  } else if (errors.isNetworkError) {
+    setErrorState(i18nextInstance.t('isNetworkError'));
+  } else {
+    switch (errors.type) {
+      case 'noRSS':
+      case 'networkError':
+        setErrorState(i18nextInstance.t(errors.type === 'noRSS' ? 'notValidRSS' : 'isNetworkError'));
         break;
       default:
-        break;
+        throw new Error('Неизвестная ошибка');
     }
+  }
+};
+
+const clearErrors = (i18nextInstance) => {
+  const errorMessage = document.querySelector('.feedback');
+  errorMessage.textContent = `${i18nextInstance.t('successRSS')}`;
+  const invalidInputs = document.querySelectorAll('.is-invalid');
+  invalidInputs.forEach((input) => input.classList.remove('is-invalid'));
+  errorMessage.classList.remove('text-danger');
+  errorMessage.classList.add('text-success');
+  const urlInput = document.querySelector('#url-input');
+  urlInput.value = '';
+  urlInput.focus();
+};
+
+function renderRssLists(rsses, state, i18nextInstance) {
+  document.querySelector('.posts').innerHTML = '';
+  document.querySelector('.feeds').innerHTML = '';
+  const divCard = document.createElement('div');
+  divCard.classList.add('card', 'border-0');
+  const divCardBody = document.createElement('div');
+  divCardBody.classList.add('card-body');
+  const divCardBodyH2 = document.createElement('h2');
+  divCardBodyH2.classList.add('card-title', 'h4');
+  divCardBodyH2.textContent = i18nextInstance.t('posts');
+  divCardBody.appendChild(divCardBodyH2);
+  const divCardUl = document.createElement('ul');
+  divCardUl.classList.add('list-group', 'border-0', 'rounded-0');
+  for (let i = rsses.length - 1; i >= 2; i -= 1) {
+    const rss = rsses[i];
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+    const a = document.createElement('a');
+    a.setAttribute('href', `${rss.link}`);
+    const aClass = state.rssForm.data.clickedListElements.has(rss.itemsId) ? 'fw-normal' : 'fw-bold';
+    a.classList.add(aClass);
+    if (state.rssForm.data.clickedListElements.has(rss.itemsId)) {
+      a.style = 'color: #6c757d';
+    }
+    a.setAttribute('data-id', `${rss.itemsId}`);
+    a.setAttribute('target', '_blank');
+    a.setAttribute('rel', 'noopener noreferrer');
+    a.textContent = `${rss.title}`;
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    button.setAttribute('data-id', `${rss.itemsId}`);
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', '#modal');
+    button.textContent = i18nextInstance.t('viewing');
+    li.appendChild(a);
+    li.appendChild(button);
+    divCardUl.appendChild(li);
+  }
+  divCard.appendChild(divCardBody);
+  divCard.appendChild(divCardUl);
+  document.querySelector('.posts').appendChild(divCard);
+  const divFeeds = document.querySelector('.feeds');
+  const divFeedsCard = document.createElement('div');
+  divFeedsCard.classList.add('card', 'border-0');
+  const divCardBody2 = document.createElement('div');
+  divCardBody2.classList.add('card-body');
+  const divCardBody2H2 = document.createElement('h2');
+  divCardBody2H2.classList.add('card-title', 'h4');
+  divCardBody2H2.textContent = i18nextInstance.t('feeds');
+  divCardBody2.appendChild(divCardBody2H2);
+  const ulFeeds = document.createElement('ul');
+  ulFeeds.classList.add('list-group', 'border-0', 'rounded-0');
+  const liFeeds = document.createElement('li');
+  liFeeds.classList.add('list-group-item', 'border-0', 'border-end-0');
+  const h3Feeds = document.createElement('h3');
+  h3Feeds.classList.add('h6', 'm-0');
+  h3Feeds.textContent = rsses[0].mainTitle;
+  const pFeeds = document.createElement('p');
+  pFeeds.classList.add('m-0', 'small', 'text-black-50');
+  pFeeds.textContent = rsses[1].mainDescription;
+  liFeeds.append(h3Feeds, pFeeds);
+  ulFeeds.appendChild(liFeeds);
+  divFeedsCard.append(divCardBody2, ulFeeds);
+  divFeeds.appendChild(divFeedsCard);
+}
+
+function appendText(elements, i18nextInstance) {
+  const currentElements = _.cloneDeep(elements);
+  currentElements.h1RuName.textContent = i18nextInstance.t('h1RuName');
+  currentElements.leadP.textContent = i18nextInstance.t('leadP');
+  currentElements.formFloatingDivLabel.textContent = i18nextInstance.t('formFloatingDivLabel');
+  currentElements.textMutedP.textContent = i18nextInstance.t('textMutedP');
+  currentElements.btn.textContent = i18nextInstance.t('btn');
+  currentElements.textCenter.textContent = i18nextInstance.t('textCenter');
+  currentElements.textCenterA.setAttribute('href', '');
+  currentElements.textCenterA.setAttribute('target', '_blank');
+  currentElements.textCenterA.textContent = i18nextInstance.t('textCenterA');
+  currentElements.textCenter.appendChild(currentElements.textCenterA);
+  currentElements.btnPrimary.textContent = i18nextInstance.t('btnPrimary');
+  currentElements.btnSecondary.textContent = i18nextInstance.t('btnSecondary');
+  currentElements.title = i18nextInstance.t('title');
+}
+
+function renderingTextModal(fData, btnIds, elements) {
+  const currentElements = _.cloneDeep(elements);
+  currentElements.modalTitle.textContent = fData.title;
+  currentElements.modalBody.textContent = fData.description;
+  currentElements.fullArticle.setAttribute('href', `${fData.link}`);
+  btnIds.forEach((btnId) => {
+    const clickedListElement = document.querySelector(`.list-group-item [data-id="${String(btnId)}"]`);
+    clickedListElement.classList.remove('fw-bold');
+    clickedListElement.classList.add('fw-normal');
+    clickedListElement.style = 'color: #6c757d';
   });
-  return watchedState;
+}
+
+export {
+  renderErrors,
+  clearErrors,
+  renderRssLists,
+  appendText,
+  renderingTextModal,
 };
